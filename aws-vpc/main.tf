9e82b09fe8d92f5998f55b945d30a02fba1d30c3
Mahesh Babu Neeli <maheshbabu22.neeli@gmail.com>
2026-03-03 22:41:46 +1100

    Terraform - Peering VPC connection

--- aws-vpc/main.tf
+++ aws-vpc/main.tf
@@ -109,7 +109,7 @@ resource "aws_route" "public" {
 }
 
 resource "aws_eip" "nat" {
-  domain   = "vpc"
+  domain = "vpc"
   tags = merge(
     local.eip_nat_final_tags,
     {
@@ -136,31 +136,31 @@ resource "aws_nat_gateway" "main" {
 
 resource "aws_route" "private" {
   route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"              // provide internet ngress access through NAT GW
-  nat_gateway_id = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0" // provide internet egress access through NAT GW
+  nat_gateway_id         = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database" {
   route_table_id         = aws_route_table.database.id
-  destination_cidr_block = "0.0.0.0/0"              // provide internet ngress access through NAT GW
-  nat_gateway_id = aws_nat_gateway.main.id
+  destination_cidr_block = "0.0.0.0/0" // provide internet egress access through NAT GW
+  nat_gateway_id         = aws_nat_gateway.main.id
 }
 
 // Associate Subnets to Route Tables
 resource "aws_route_table_association" "public" {
-  count = length(var.public_subnet_cidrs)
+  count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "private" {
-  count = length(var.private_subnet_cidrs)
+  count          = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
 
 resource "aws_route_table_association" "database" {
-  count = length(var.database_subnet_cidrs)
+  count          = length(var.database_subnet_cidrs)
   subnet_id      = aws_subnet.database[count.index].id
   route_table_id = aws_route_table.database.id
 }
